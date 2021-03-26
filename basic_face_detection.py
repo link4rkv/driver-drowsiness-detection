29a71df63ec60ca31159145cb3068dd0994e7f79
link4rkv <link4rkv@gmail.com>
2021-03-27 01:06:17 +0530

    adding basic eyes detection

--- basic_face_detection.py
+++ basic_face_detection.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 
-face_cascade = cv.CascadeClassifier('haarcascade_frontalface_alt.xml')
+face_cascade = cv.CascadeClassifier('cascade files/haarcascade_frontalface_alt.xml')
+eyes_cascade = cv.CascadeClassifier('cascade files/haarcascade_eye_tree_eyeglasses.xml')
 
 cap = cv.VideoCapture(-1)
 
@@ -16,11 +17,16 @@ while True:
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     frame_gray = cv.equalizeHist(frame_gray)
 
-    faces = face_cascade.detectMultiScale(frame_gray)
+    faces = face_cascade.detectMultiScale(frame_gray,minNeighbors=5,scaleFactor=1.1,minSize=(25,25))
     for (x,y,w,h) in faces:
         center = (x + w//2, y + h//2)
         frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
         faceROI = frame_gray[y:y+h,x:x+w]
+        eyes = eyes_cascade.detectMultiScale(faceROI)
+        for (x2,y2,w2,h2) in eyes:
+            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
+            radius = int(round((w2 + h2)*0.25))
+            frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
         
     cv.imshow('Capture - Face detection', frame)
     if cv.waitKey(10) == 27:
